3b63809a8ab3825c3e6073ef95f8ef1cb2e601c8
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-07-19 20:01:42 +0700

    add swipe gesture for carousel

--- js/custom.js
+++ js/custom.js
@@ -369,4 +369,13 @@ $(function () {
      });
 
 
+	/* Carousel Swipe Gesture
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$(".carousel").swipe({
+		swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+			if (direction == "left") $(this).carousel('next');
+			if (direction == "right") $(this).carousel('prev');
+		},
+		allowPageScroll:"vertical"
+	});
 });
